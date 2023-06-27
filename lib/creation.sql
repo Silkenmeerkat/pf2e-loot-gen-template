8935178d3049c2ede8e3909d0f7bdff416a0df55
Cameron Burt <silkenmeerkat@mgmail.com>
2023-06-26 22:50:08 -0400

    Going to sleep

---
+++ lib/creation.sql
@@ -0,0 +1,38 @@
+-- Create the items table
+CREATE TABLE items (
+  item_id SERIAL PRIMARY KEY,
+  img VARCHAR(255),
+  name VARCHAR(255),
+  system_id INT,
+  type VARCHAR(255),
+  FOREIGN KEY (system_id) REFERENCES system(system_id)
+);
+
+-- Create the traits table
+CREATE TABLE traits (
+  trait_id SERIAL PRIMARY KEY,
+  trait_name VARCHAR(255)
+);
+
+-- Create the item_traits table
+CREATE TABLE item_traits (
+  item_id INT,
+  trait_id INT,
+  FOREIGN KEY (item_id) REFERENCES items(item_id),
+  FOREIGN KEY (trait_id) REFERENCES traits(trait_id),
+  PRIMARY KEY (item_id, trait_id)
+);
+
+-- Create the system table
+CREATE TABLE system (
+  system_id INT PRIMARY KEY,
+  description_value TEXT,
+  bulk INT,
+  level_value INT,
+  precious_material_value VARCHAR(255),
+  precious_material_grade_value VARCHAR(255),
+  price_gp_value INT,
+  quantity_per_batch INT,
+  source_book VARCHAR(255),
+  rarity_value VARCHAR(255),
+);
