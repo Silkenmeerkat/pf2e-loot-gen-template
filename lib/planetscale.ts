2d23a329749482ae2728f2adebccb2cf283dbb3b
Cameron Burt <silkenmeerkat@mgmail.com>
2023-06-26 22:50:15 -0400

    sleep

--- lib/planetscale.ts
+++ lib/planetscale.ts
@@ -2,20 +2,31 @@ import 'server-only';
 import { Generated, Kysely } from 'kysely';
 import { PlanetScaleDialect } from 'kysely-planetscale';
 
-interface User {
-  id: Generated<number>;
-  name: string;
-  username: string;
-  email: string;
-}
+// interface item {
+//   id: Generated<number>;
+//   name: string;
+//   description: string;
+//   bulk: string;
+//   price: Float32Array; 
+//   quantity: Int32Array;
+//   source: string;
+//   type: string;
+//   level: Int16Array
+//   rarity: string;
+//   //traits TBD?
+// }
+// 
+// CREATE TABLE items (
+//   item_id INT PRIMARY KEY AUTO_INCREMENT,
+//   img VARCHAR(255),
+//   name VARCHAR(255),
+//   system_id INT,
+//   type VARCHAR(255),
+//   FOREIGN KEY (system_id) REFERENCES system(system_id)
+// );
+// interface items {
+//   item_id: Generated<number>
+//   name: string; 
+//   system_id: Int;
 
-interface Database {
-  users: User;
-  // https://github.com/nextauthjs/next-auth/issues/4922
-}
-
-export const queryBuilder = new Kysely<Database>({
-  dialect: new PlanetScaleDialect({
-    url: process.env.DATABASE_URL
-  })
-});
+// }
---
+++ lib/wherewasi.md
@@ -0,0 +1,11 @@
+# What we did 
+1. Learned about Kysely https://kysely-org.github.io/kysely/#installation
+2. learned about ORM's a bit more 
+3. Designed a schema 
+4. Got local ps set up
+5. Implemented probably correct migration up and downs in kysely
+# What we do next
+1. figure out how to write a migration class instance
+2. Figure out how to point it at the local db
+3. Figure out how to run the up. 
+4. Start on ingestion engine! 
